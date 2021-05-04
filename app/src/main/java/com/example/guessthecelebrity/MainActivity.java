cb45869121835ff88ec3f8d7cefd0df1263335c6
zobaer53 <61627836+zobaer53@users.noreply.github.com>
2021-05-05 00:25:16 +0600

    1st

---
+++ app/src/main/java/com/example/guessthecelebrity/MainActivity.java
@@ -0,0 +1,219 @@
+package com.example.guessthecelebrity;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class MainActivity extends AppCompatActivity {
+
+
+
+
+    ArrayList<String> celebURLs = new ArrayList<String>();
+    ArrayList<String> celebNames = new ArrayList<String>();
+    int chosenCeleb = 0;
+    String[] answers = new String[4];
+    int locationOfCorrectAnswer = 0;
+    ImageView imageView;
+    Button button0;
+    Button button1;
+    Button button2;
+    Button button3;
+
+    public void celebChosen(View view) {
+        Log.i("bal","celebchosen");
+        if (view.getTag().toString().equals(Integer.toString(locationOfCorrectAnswer))) {
+            Toast.makeText(getApplicationContext(),"Correct!",Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(getApplicationContext(),"Worng! It was " + celebNames.get(chosenCeleb),Toast.LENGTH_SHORT).show();
+        }
+
+        newQuestion();
+    }
+
+    public class ImageDownloader extends AsyncTask<String, Void, Bitmap> {
+
+        @Override
+        protected Bitmap doInBackground(String... urls) {
+            try {
+                Log.i("bal","imagedownloader");
+                URL url = new URL(urls[0]);
+
+                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+                connection.connect();
+                InputStream inputStream = connection.getInputStream();
+                Bitmap myBitmap = BitmapFactory.decodeStream(inputStream);
+                return myBitmap;
+
+            } catch (Exception e) {
+                e.printStackTrace();
+                return null;
+            }
+        }
+    }
+
+    public class DownloadTask extends AsyncTask<String, Void, String> {
+
+        @Override
+        protected String doInBackground(String... urls) {
+
+            String result = "";
+            URL url;
+            HttpURLConnection urlConnection = null;
+
+            try {
+                Log.i("bal","downloadTask");
+
+                url = new URL(urls[0]);
+
+                urlConnection = (HttpURLConnection) url.openConnection();
+
+                InputStream in = urlConnection.getInputStream();
+
+                InputStreamReader reader = new InputStreamReader(in);
+
+                int data = reader.read();
+
+                while (data != -1) {
+                    char current = (char) data;
+                    result += current;
+                    data = reader.read();
+                }
+
+                return result;
+
+            } catch (Exception e) {
+                e.printStackTrace();
+                return null;
+            }
+
+        }
+    }
+
+    public void newQuestion() {
+        try {
+            Log.i("bal","newQuestion");
+
+            Random rand = new Random();
+
+            chosenCeleb = rand.nextInt(celebURLs.size());
+
+            ImageDownloader imageTask = new ImageDownloader();
+
+            Bitmap celebImage = imageTask.execute(celebURLs.get(chosenCeleb)).get();
+
+            imageView.setImageBitmap(celebImage);
+
+            locationOfCorrectAnswer = rand.nextInt(4);
+
+            int incorrectAnswerLocation;
+
+            for (int i = 0; i < 4; i++) {
+                if (i == locationOfCorrectAnswer) {
+                    answers[i] = celebNames.get(chosenCeleb);
+                } else {
+                    incorrectAnswerLocation = rand.nextInt(celebURLs.size());
+
+                    while (incorrectAnswerLocation == chosenCeleb) {
+                        incorrectAnswerLocation = rand.nextInt(celebURLs.size());
+                    }
+
+                    answers[i] = celebNames.get(incorrectAnswerLocation);
+                }
+            }
+
+            button0.setText(answers[0]);
+            button1.setText(answers[1]);
+            button2.setText(answers[2]);
+            button3.setText(answers[3]);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        imageView = findViewById(R.id.imageView);
+        button0 = findViewById(R.id.button0);
+        button1 = findViewById(R.id.button1);
+        button2 = findViewById(R.id.button2);
+        button3 = findViewById(R.id.button3);
+
+        DownloadTask task = new DownloadTask();
+        String result = null;
+        Log.i("bal","Works");
+
+        new Thread(new Runnable()
+        {
+            //final EbayProduct product = new EbayProduct();
+            @Override
+            public void run()
+            {
+                try
+                {
+                    String url = "https://www.celebritynetworth.com/list/top-50-richest-celebrities/";
+
+
+
+                    Document doc = Jsoup.connect(url).get();
+
+                    Elements body = doc.select("ul#top_100_list.top_100_couples.main_padding");
+                    //Elements img = body.select("img");
+
+                    //System.out.println(body.select("tr").size());
+                    Log.i("Size",Integer.toString(body.size()));
+                    for(Element e : body.select("li"))
+                    {
+
+
+
+                        String title = e.select("div.thumb img").attr("alt");
+                        String image = e.select("div.thumb img").attr("data-src");
+                        celebURLs.add(image);
+                        celebNames.add(title);
+                        Log.i("bal", String.valueOf(celebURLs));
+                        Log.i("bal2", String.valueOf(celebNames));
+                    }
+
+                    newQuestion();
+
+
+
+
+
+                }catch (Exception ex){
+   ex.printStackTrace();
+                }
+
+
+            }
+        }).start();
+
+
+    }
+}
